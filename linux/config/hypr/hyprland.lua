6c63aae6a12f1613aa0ef124553621200b068960
estebaninfante <juanesteban.infantem@gmail.com>
2026-09-01 15:11:08 -0500

    auto: 2026-09-01 15:11 |  1 file changed, 1 insertion(+)

--- linux/config/hypr/hyprland.lua
+++ linux/config/hypr/hyprland.lua
@@ -77,6 +77,7 @@ hl.config({
     input = {
         kb_layout   = "dvk_prog,es,us",
         kb_variant  = "basic,,",
+        kb_options  = "caps:none",
         follow_mouse = 1,
         sensitivity = 0
     }
