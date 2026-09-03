46c94f8b765b803994d3ebe125f6d01ca0b8ae5b
estebaninfante <juanesteban.infantem@gmail.com>
2026-09-02 20:41:44 -0500

    auto: 2026-09-02 20:41 |  1 file changed, 6 insertions(+), 1 deletion(-)

--- linux/config/nvim/lua/plugins/live-server.lua
+++ linux/config/nvim/lua/plugins/live-server.lua
@@ -2,6 +2,11 @@ return {
   "barrett-ruth/live-server.nvim",
   cmd = { "LiveServerStart", "LiveServerStop" },
   init = function()
-    vim.g.live_server = { port = 8080 }
+    vim.g.live_server = {
+      port = 8080,
+      root = ".",
+      open = false,
+      wait = 100,
+    }
   end,
 }
